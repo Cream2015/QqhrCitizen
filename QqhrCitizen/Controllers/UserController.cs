fe42a3beab2cb324d0e25515e155d06b20d473aa
applenele <1941973283@qq.com>
2015-06-05 19:07:12 +0800

    666

--- QqhrCitizen/Controllers/UserController.cs
+++ QqhrCitizen/Controllers/UserController.cs
@@ -21,7 +21,14 @@ namespace QqhrCitizen.Controllers
             return View();
         }
 
+        #region 注册
+        /// <summary>
+        /// 注册
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Register(vRegister model)
         {
 
@@ -29,7 +36,7 @@ namespace QqhrCitizen.Controllers
             user1 = db.Users.Where(u => u.Username == model.Username).SingleOrDefault();
             if (user1 == null)
             {
-                User user = new User { Username = model.Username, Password = Helpers.Encryt.GetMD5(model.Password), Role = Role.User,Realname=model.Realname,Email=model.Email,Phone=model.Phone };
+                User user = new User { Username = model.Username, Password = Helpers.Encryt.GetMD5(model.Password), Role = Role.User, Realname = model.Realname, Email = model.Email, Phone = model.Phone };
                 user.Birthday = Convert.ToDateTime("2012-1-1");
                 db.Users.Add(user);
                 int result = db.SaveChanges();
@@ -47,7 +54,8 @@ namespace QqhrCitizen.Controllers
                 ModelState.AddModelError("", "用户名不可用！");
             }
             return View();
-        }
+        } 
+        #endregion
 
         [HttpGet]
         public ActionResult Login()
@@ -81,25 +89,49 @@ namespace QqhrCitizen.Controllers
             }
             return View(model);
         }
+
+        #region show
         /// <summary>
         /// show
         /// </summary>
         /// <param name="id"></param>
         /// <returns></returns>
-        public  ActionResult Show(int id)
+        [BaseAuth(Roles = "User")]
+        [HttpGet]
+        public ActionResult Show(int id)
         {
             User user = new User();
             user = db.Users.Find(id);
             ViewBag.user = new vUser(user);
             return View();
-        }
+        } 
+        #endregion
+
+        #region 修改个人西信息
+        /// <summary>
+        /// 修改个人西信息
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [BaseAuth(Roles = "User")]
         [HttpGet]
         public ActionResult Edit(int id)
         {
             User user = db.Users.Find(id);
             ViewBag.user = user;
             return View();
-        }
+        } 
+        #endregion
+
+        #region 修改个人西信息
+        /// <summary>
+        /// 修改个人西信息
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [BaseAuth(Roles = "User")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Edit(vUserEdit model)
         {
             User user1 = db.Users.Find(model.ID);
@@ -113,17 +145,17 @@ namespace QqhrCitizen.Controllers
             //user.Email = model.Email;
             //user.Picture = model.Picture;
             //user.Realname = model.Realname;
-            
+
 
             if (ModelState.IsValid)
             {
-                 User user = db.Users.Find(model.ID);
+                User user = db.Users.Find(model.ID);
 
                 //user.Username = model.Username;
                 //FormsAuthentication.SignOut();
 
                 FormsAuthentication.SetAuthCookie(model.Username, false);
-                
+
                 if (!string.IsNullOrEmpty(model.Password))
                 {
                     if (!user.Password.Equals(Helpers.Encryt.GetMD5(model.Password)))
@@ -148,7 +180,8 @@ namespace QqhrCitizen.Controllers
                 ModelState.AddModelError("", "修改的信息输入错误!");
             }
             return View(model);
-        }
+        } 
+        #endregion
 
          #region 注销
 
@@ -156,6 +189,7 @@ namespace QqhrCitizen.Controllers
         ///  注销 
         /// </summary>
         /// <returns></returns>
+        [HttpGet]
         public ActionResult Logout()
         {
             FormsAuthentication.SignOut();
