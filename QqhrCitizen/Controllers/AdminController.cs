20e467c7a625eec06b5fc861fdb52c7538047d3f
applenele <1941973283@qq.com>
2015-06-05 15:27:42 +0800

    66

---
+++ QqhrCitizen/Controllers/AdminController.cs
@@ -0,0 +1,39 @@
+﻿using QqhrCitizen.Models.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QqhrCitizen.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        #region 后台管理登陆
+        /// <summary>
+        /// 后台管理登陆
+        /// </summary>
+        /// <returns></returns>
+        [Filters.BaseAuth(Roles = "Admin")]
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        } 
+        #endregion
+
+      
+        [HttpPost]
+        public ActionResult Login(vAdminModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ QqhrCitizen/Filters/BaseAuthAttribute.cs
@@ -0,0 +1,33 @@
+﻿using QqhrCitizen.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QqhrCitizen.Filters
+{
+    public class BaseAuthAttribute : AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            DB db = new DB();
+            User user = db.Users.Where(u => u.Username == HttpContext.Current.User.Identity.Name).FirstOrDefault();
+            if (user != null)
+            {
+                if (Roles.Contains(user.Role.ToString()))
+                {
+                    return true;
+                }
+                else
+                {
+                    return base.AuthorizeCore(httpContext);
+                }
+            }
+            else 
+            {
+                return base.AuthorizeCore(httpContext);
+            }
+        }
+    }
+}
