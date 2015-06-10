ba06dc918cf3b94fda0642b98fcd470f6c6d1187
applenele <1941973283@qq.com>
2015-06-11 05:46:24 +0800

    666

--- QqhrCitizen/Controllers/BaseController.cs
+++ QqhrCitizen/Controllers/BaseController.cs
@@ -20,14 +20,21 @@ namespace QqhrCitizen.Controllers
             List<TypeDictionary> newsTypes = new List<TypeDictionary>();
             List<vTypeDictionary> _newsTypes = new List<vTypeDictionary>();
 
+            List<TypeDictionary> courseTypes = new List<TypeDictionary>();
+            List<vTypeDictionary> _courseTypes = new List<vTypeDictionary>();
+
+            List<TypeDictionary> ebookTypes = new List<TypeDictionary>();
+            List<vTypeDictionary> _ebookTypes = new List<vTypeDictionary>();
+
+
             List<News> news = new List<News>();
             List<vNews> _news = new List<vNews>();
 
             List<Course> courses = new List<Course>();
             List<vCourse> _courses = new List<vCourse>();
 
-            List<TypeDictionary> courseTypes = new List<TypeDictionary>();
-            List<vTypeDictionary> _courseTypes = new List<vTypeDictionary>();
+            List<EBook> books = new List<EBook>();
+            List<vEBook> _books = new List<vEBook>();
 
             List<ResourceLink> nflinks = new List<ResourceLink>();
             List<ResourceLink> flinks = new List<ResourceLink>();
@@ -44,6 +51,12 @@ namespace QqhrCitizen.Controllers
                 _courseTypes.Add(new vTypeDictionary(type));
             }
 
+            ebookTypes = db.TypeDictionaries.Where(td => td.FatherID == 0 && td.Belonger == TypeBelonger.EBook).ToList();
+            foreach (var type in ebookTypes)
+            {
+                _ebookTypes.Add(new vTypeDictionary(type));
+            }
+
             #region 最新5条新闻
             news = db.News.OrderByDescending(n => n.Time).Take(5).ToList();
             foreach (var item in news)
@@ -57,7 +70,15 @@ namespace QqhrCitizen.Controllers
             foreach (var item in courses)
             {
                 _courses.Add(new vCourse(item));
-            } 
+            }
+            #endregion
+
+            #region 最新的5门图书
+            books = db.EBooks.OrderByDescending(c => c.Time).Take(5).ToList();
+            foreach (var item in books)
+            {
+                _books.Add(new vEBook(item));
+            }
             #endregion
 
             #region 资源连接
@@ -69,13 +90,17 @@ namespace QqhrCitizen.Controllers
             } 
             #endregion
 
-            
+
+
+           
 
 
             ViewBag.NewsTypes = _newsTypes;
             ViewBag.CourseTypes = _courseTypes;
+            ViewBag.EBookTypes = _ebookTypes;
             ViewBag.News = _news;
             ViewBag.Courses = _courses;
+            ViewBag.EBooks = _books;
             ViewBag.NfLinks = nflinks;
             ViewBag.FLinks = vflinks;
             if (requestContext.HttpContext.User != null && requestContext.HttpContext.User.Identity.IsAuthenticated)
