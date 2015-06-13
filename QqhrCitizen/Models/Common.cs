85dc3905383d504657537108620d771599363b44
applenele <1941973283@qq.com>
2015-06-14 01:02:05 +0800

    2

--- QqhrCitizen/Models/Common.cs
+++ QqhrCitizen/Models/Common.cs
@@ -8,12 +8,12 @@ namespace QqhrCitizen.Models
     public enum TypeBelonger { 新闻, 文件, 资源链接, 电子书, 课程 }
     public enum Authority { User, Admin }
     public enum Sex { 女, 男 }
-    public enum Role { User, Admin,NewsManager,EBookManager,CourseManager }
+    public enum Role { User, Admin,NewsManager,EBookManager,CourseManager,LinkManager }
 
     public class CommonEnums
     {
         public static string[] Answers = { "A", "B", "C", "D"};
 
-        public static string[] RoleDisply = { "用户", "超级管理员", "新闻管理员", "图书管理员", "课程管理员" };
+        public static string[] RoleDisply = { "用户", "超级管理员", "新闻管理员", "图书管理员", "课程管理员","资源连接管理员" };
     }
 }
