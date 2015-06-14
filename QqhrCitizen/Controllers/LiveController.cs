c0410d28c6eab1489a3692bc1ee9fecf7921f836
applenele <1941973283@qq.com>
2015-06-14 19:22:23 +0800

    6

--- QqhrCitizen/Controllers/LiveController.cs
+++ QqhrCitizen/Controllers/LiveController.cs
@@ -17,6 +17,7 @@ namespace QqhrCitizen.Controllers
 
         public ActionResult Channel(int id)
         {
+            ViewBag.ChananelID = id;
             return View();
         }
 	}
--- QqhrCitizen/SignalR/ContosoChatHub.cs
+++ QqhrCitizen/SignalR/ContosoChatHub.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
 
 namespace QqhrCitizen.SignalR
 {
     public class ContosoChatHub : Hub
     {
-        public void NewContosoChatMessage(string name, string message)
+        public void NewContosoChatMessage(string name, string message,string cid)
         {
-            Clients.All.addContosoChatMessageToPage(name, message);
+           // Clients.All.addContosoChatMessageToPage(name, message);
+            Clients.Group(cid).addContosoChatMessageToPage(name, message);
+        }
+
+        public async Task JoinGroup(string groupname)
+        {
+            await Groups.Add(Context.ConnectionId, groupname);
+           // Clients.Group(groupname).addGroupToShow(Context.ConnectionId + "add to group");
+        }
+
+        public async Task LeaveGroup(string groupname)
+        {
+            await Groups.Remove(Context.ConnectionId, groupname);
+
+            Clients.User(Context.ConnectionId).removeGroupToShow(Context.ConnectionId + "remove to group");
         }
     }
 }
