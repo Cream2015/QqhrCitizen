521539d299f6d5f87afcd8fb9ee0d3db179b01e7
applenele <1941973283@qq.com>
2015-06-25 14:52:33 +0800

    1

--- QqhrCitizen/Scripts/citizen.js
+++ QqhrCitizen/Scripts/citizen.js
@@ -219,6 +219,9 @@ $(document).ready(function () {
         }
         var str = "";
         var answers = $(".answer:checked");
+        if (answers.length==0) {
+            CastMsg("你有题目没有选择答案，请先完成答题在提交！");
+        }
         var count = 0;
         var rate = 1.0;
         var questionCount = 0;
