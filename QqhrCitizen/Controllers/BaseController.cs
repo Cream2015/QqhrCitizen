0b0d9fa2719254164cd205bd493bd2d07f276dd0
志俊 <fanfzj@163.com>
2015-06-27 00:02:40 +0800

    注册

--- QqhrCitizen/Controllers/BaseController.cs
+++ QqhrCitizen/Controllers/BaseController.cs
@@ -60,7 +60,7 @@ namespace QqhrCitizen.Controllers
             ViewBag.NewsTypes = _newsTypes;
             ViewBag.CourseTypes = _courseTypes;
             ViewBag.EBookTypes = _ebookTypes;
-            if (requestContext.HttpContext.User != null && requestContext.HttpContext.User.Identity.IsAuthenticated)
+            if (requestContext.HttpContext.User.Identity.IsAuthenticated)
             {
                 ViewBag.CurrentUser = (from u in db.Users
                                        where u.Username == requestContext.HttpContext.User.Identity.Name
