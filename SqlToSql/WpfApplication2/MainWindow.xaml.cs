2dba68a71c81b5abbc0413461e2033b0e7b92a39
fanfzj <fanfzj@163.com>
2015-06-28 08:04:45 +0800

    更新导入地址，变迁为42.96.129.28，重写导入规则

--- SqlToSql/WpfApplication2/MainWindow.xaml.cs
+++ SqlToSql/WpfApplication2/MainWindow.xaml.cs
@@ -16,7 +16,7 @@ using System.Data.SqlClient;
 using System.Data;
 using System.Globalization;
 
-namespace WpfApplication2 
+namespace WpfApplication2
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
@@ -31,7 +31,7 @@ namespace WpfApplication2
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Sql_list.Items.Clear();
+            ShowSqlNumber.Text = "";
             string qqhrstudy_sql = "select * from NewsInfo";
             string citizen_sql;
             DataTable citizen_sqldt = qqhr_sqlhelp.ExecuteDataTable(qqhrstudy_sql);
@@ -39,7 +39,7 @@ namespace WpfApplication2
             for (int i = 0; i < citizen_sqldt.Rows.Count; i++)
             {
                 string Time = Convert.ToDateTime(citizen_sqldt.Rows[i]["CreatedTime"]).ToString("yyyy-MM-dd HH:mm:ss.fff", DateTimeFormatInfo.InvariantInfo);
-                citizen_sql = "insert into News (Title,Content,FirstImgUrl,Time,UserID,Browses,NewsTypeID,IsHaveImg) values (@Title,@Content,@FirstImgUrl,@Time,@UserID,@Browse,@NewsTypeID,@IshaveImg)";
+                citizen_sql = "insert into News (Title,Content,FirstImgUrl,Time,UserID,Browses,NewsTypeID,IsHaveImg,IsWord) values (@Title,@Content,@FirstImgUrl,@Time,@UserID,@Browse,@NewsTypeID,@IshaveImg,@IsWord)";
                 SqlParameter[] para = new SqlParameter[]
 	                    {
                              new SqlParameter("@Title", SqlDbType.VarChar),
@@ -49,10 +49,11 @@ namespace WpfApplication2
                              new SqlParameter("@UserID", SqlDbType.Int),
                              new SqlParameter("@Browse",SqlDbType.Int),
                              new SqlParameter("@NewsTypeID", SqlDbType.Int),
-                             new SqlParameter("@IshaveImg",SqlDbType.Int)
+                             new SqlParameter("@IshaveImg",SqlDbType.Int),
+                             new SqlParameter("@IsWord",SqlDbType.Int)
                         };
                 para[0].Value = citizen_sqldt.Rows[i]["Subject"].ToString();
-                if (citizen_sqldt.Rows[i]["ImageFlag"].ToString()!="0")
+                if (citizen_sqldt.Rows[i]["ImageFlag"].ToString() != "0")
                 {
                     para[1].Value = "<img src='/" + citizen_sqldt.Rows[i]["ImageUrl"].ToString() + "'>" + citizen_sqldt.Rows[i]["MessageBody"].ToString();
                 }
@@ -64,17 +65,19 @@ namespace WpfApplication2
                 para[3].Value = Time;
                 para[4].Value = 1;
                 para[5].Value = 0;
-                para[6].Value = 37;
+                para[6].Value = 5;
+                //para[6].Value = 37;
                 para[7].Value = citizen_sqldt.Rows[i]["ImageFlag"].ToString();
-                a += citizen_sqlhelp.ExecuteNonQuery(citizen_sql,para);
-                Sql_list.Items.Add(citizen_sql);
+                para[8].Value = 0;
+                a += citizen_sqlhelp.ExecuteNonQuery(citizen_sql, para);
+                ShowSqlNumber.Text = ShowSqlNumber.Text + "<br>" + citizen_sql;
             }
-            MessageBox.Show("导入数据："+a.ToString());
+            MessageBox.Show("导入数据：" + a.ToString());
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            Sql_list.Items.Clear();
+            ShowSqlNumber.Text = "";
             string qqhrstudy_sql = "select * from CourseInfo";
             string citizen_sql;
             DataTable citizen_sqldt = qqhr_sqlhelp.ExecuteDataTable(qqhrstudy_sql);
@@ -100,12 +103,57 @@ namespace WpfApplication2
                 para[3].Value = Time;
                 para[4].Value = 1;
                 para[5].Value = 0;
-                para[6].Value = 37;
+                para[6].Value = 5;
+                //para[6].Value = 37;
                 para[7].Value = citizen_sqldt.Rows[i]["CreditHour"];
-                a += citizen_sqlhelp.ExecuteNonQuery(citizen_sql, para);
-                Sql_list.Items.Add(citizen_sql);
+                int insert_id = Convert.ToInt32(citizen_sqlhelp.ExecuteScalar(citizen_sql + ";Select @@Identity", para));
+                string course_sql = "select * from CoursewareInfo where CourseId=" + citizen_sqldt.Rows[i]["Id"];
+                DataTable coursest = qqhr_sqlhelp.ExecuteDataTable(course_sql);
+                if (coursest.Rows.Count > 0)
+                {
+                    //string insert_sql = "insert into Lessions (Title,Description,CourseID,Time,Remark,Path,Browses,IsPassTest) values ('" + coursest.Rows[0]["Name"] + "','" + coursest.Rows[0]["Name"] + "','" + insert_id + "','" + Convert.ToDateTime(coursest.Rows[0]["CreatedTime"]).ToString("yyyy-MM-dd HH:mm:ss.fff", DateTimeFormatInfo.InvariantInfo) + "','" + coursest.Rows[0]["Name"] + "','" + coursest.Rows[0]["Url"] + "','0','0')";
+                    string insert_sql = "insert into Lessions (Title,Description,CourseID,Time,Remark,Path,Browses) values ('" + coursest.Rows[0]["Name"] + "','" + coursest.Rows[0]["Name"] + "','" + insert_id + "','" + Convert.ToDateTime(coursest.Rows[0]["CreatedTime"]).ToString("yyyy-MM-dd HH:mm:ss.fff", DateTimeFormatInfo.InvariantInfo) + "','" + coursest.Rows[0]["Name"] + "','" + coursest.Rows[0]["Url"] + "','0')";
+                    citizen_sqlhelp.ExecuteNonQuery(insert_sql);
+                }
+                a++;
+                ShowSqlNumber.Text = ShowSqlNumber.Text+"<br>" + course_sql;
             }
             MessageBox.Show("导入数据：" + a.ToString());
+        }
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            /*Sql_list.Items.Clear();
+            string qqhrstudy_sql = "select * from CoursewareInfo";
+            string citizen_sql;
+            DataTable citizen_sqldt = qqhr_sqlhelp.ExecuteDataTable(qqhrstudy_sql);
+            int a = 0;
+            for (int i = 0; i < citizen_sqldt.Rows.Count; i++)
+            {
+                string Time = Convert.ToDateTime(citizen_sqldt.Rows[i]["CreatedTime"]).ToString("yyyy-MM-dd HH:mm:ss.fff", DateTimeFormatInfo.InvariantInfo);
+                citizen_sql = "insert into Lessions (Title,Description,CourseID,Time,Remark,Path,Browses,IsPassTest) values (@Title,@Description,@CourseId,@Time,@Remark,@Path,@Browses,@IsPassTest)";
+                SqlParameter[] para = new SqlParameter[]
+	                    {
+                             new SqlParameter("@Title", SqlDbType.VarChar),
+                             new SqlParameter("@Description",SqlDbType.VarChar),
+                             new SqlParameter("@CourseID", SqlDbType.Int),
+                             new SqlParameter("@Time",SqlDbType.DateTime),
+                             new SqlParameter("@Remark", SqlDbType.VarChar),
+                             new SqlParameter("@Path",SqlDbType.VarChar),
+                             new SqlParameter("@Browses", SqlDbType.Int),
+                             new SqlParameter("@IsPassTest",SqlDbType.Int)
+                        };
+                para[0].Value = citizen_sqldt.Rows[i]["Name"].ToString();
+                para[1].Value = citizen_sqldt.Rows[i]["Name"].ToString();
+                para[2].Value = citizen_sqldt.Rows[i]["CourseID"].ToString();
+                para[3].Value = Time;
+                para[4].Value = citizen_sqldt.Rows[i]["Name"].ToString();
+                para[5].Value = citizen_sqldt.Rows[i]["Url"].ToString();
+                para[6].Value = 0;
+                para[7].Value = 0;
+                a += citizen_sqlhelp.ExecuteNonQuery(citizen_sql, para);
+                Sql_list.Items.Add(citizen_sql);
+            }*/
+            MessageBox.Show("导入数据");
         }
     }
 }
