c904a9b1616a156d688b630c72b0b6f5369d8aee
applenele <1941973283@qq.com>
2015-06-29 08:14:19 +0800

    1
    2

---
+++ QqhrCitizen/Controllers/ViewPagerController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QqhrCitizen.Controllers
+{
+    public class ViewPagerController : BaseController
+    {
+        // GET: ViewPager
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ShowPicture(int id)
+        {
+            Models.Viewpager view = new Models.Viewpager();
+            view = db.Viewpagers.Find(id);
+            return File(view.Picture, "image/jpg");
+        }
+    }
+}
