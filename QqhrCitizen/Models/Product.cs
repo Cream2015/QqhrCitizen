c15abea14fdc98a1fbe61364631353796abbb47f
applenele <1941973283@qq.com>
2015-07-03 11:03:46 +0800

    2

---
+++ QqhrCitizen/Models/Product.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace QqhrCitizen.Models
+{
+    public class Product
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public int ID { set; get; }
+
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// 描述
+        /// </summary>
+        public string Description { set; get; }
+
+        /// <summary>
+        /// 价格
+        /// </summary>
+        public string Price { set; get; }
+
+        /// <summary>
+        ///  增加者的ID
+        /// </summary>
+        [ForeignKey("User")]
+        public int UserID { set; get; }
+
+        public virtual User User { set; get; }
+
+        /// <summary>
+        /// 发布日期
+        /// </summary>
+        public DateTime Time { set; get; }
+    }
+}
---
+++ QqhrCitizen/Models/ProductFile.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace QqhrCitizen.Models
+{
+    /// <summary>
+    ///  产品图片
+    /// </summary>
+    public class ProductFile
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public int ID { set; get; }
+
+        /// <summary>
+        ///  产品ID
+        /// </summary>
+        [ForeignKey("Product")]
+        public int ProductID { set; get; }
+
+
+        /// <summary>
+        /// 文件路径
+        /// </summary>
+        public string Path { set; get; }
+
+        /// <summary>
+        /// 文件类型
+        /// </summary>
+        public int FileTypeAsInt { set; get; }
+
+        [NotMapped]
+        public ProductFileType ProductFileType
+        {
+           set { FileTypeAsInt = (int)value; }
+            get { return (ProductFileType)FileTypeAsInt; }
+        }
+    }
+
+    public enum ProductFileType { image,video}
+}
