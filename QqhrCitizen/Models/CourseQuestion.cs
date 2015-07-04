15c9dbcabc04a545bf27bda3284eb851e5d803b6
fanfzj <fanfzj@163.com>
2015-07-04 15:55:38 +0800

    新建 课程测试

---
+++ QqhrCitizen/Models/CourseQuestion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace QqhrCitizen.Models
+{
+    public class CourseQuestion
+    {
+        /// <summary>
+        /// 课程问题 by JX123
+        /// </summary>
+            public int ID { get; set; }
+
+            public string Content { get; set; }
+
+            public string Answers { get; set; }
+
+            public string Remark { get; set; }
+
+            [StringLength(2)]
+            public string RightAnswer { get; set; }
+
+            public DateTime Time { get; set; }
+
+            [ForeignKey("Course")]
+            public int CourseID { get; set; }
+            public virtual Course Course { get; set; }
+    }
+}
